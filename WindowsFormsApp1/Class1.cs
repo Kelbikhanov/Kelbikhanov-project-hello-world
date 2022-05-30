44a78b55fd8d4443c02961a456e248b35f547450
kelbikhanov <rusik9091@gmail.com>
2022-05-30 09:39:10 +0300

    test commit

---
+++ WindowsFormsApp1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Class1
+    {
+        static void main(string[]  args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
